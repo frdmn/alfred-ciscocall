a9bfda20d1e8d0fb894cd62d693e1e555fd45136
Jonas Friedmann <j@frd.mn>
2014-09-04 13:48:27 +0200

    Add call helper

---
+++ call.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Read configuration/settings
+source ${HOME}/.alfred-ciscocall.conf
+
+# Store arguments in variable and replace spaces
+PHONENUMBER=${*// /}
+
+if [[ -z ${CISCOIP} || -z ${CISCOUSER} || -z ${CISCOPASS} || -z ${CISCOENDPOINT} ]]; then
+    echo "Error reading environment variables. Check '~/.alfred-ciscocall.conf'"
+    exit 1
+fi
+
+# Display output
+RAWRESPONSE=$(curl -s --data 'XML=<CiscoIPPhoneExecute><ExecuteItem URL="Dial:'${PHONENUMBER}'" /></CiscoIPPhoneExecute>' http://ss${CISCOUSER}:${CISCOPASS}@${CISCOIP}:${CISCOPORT}/CGI/Execute)
+STATUSCODE=$?
+
+# Check exit code of curl command
+if [[ ${STATUSCODE} -ne 0 ]]; then
+    echo "Error while establish phone call"
+fi
+
+if [[ $(echo "${RAWRESPONSE}") =~ "Error" ]]; then
+    echo "Error in response: \"${RAWRESPONSE}\""
+fi
